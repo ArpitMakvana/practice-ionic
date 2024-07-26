1879209a0148ac422148d29754042f48ee39c7dd
ArpitMakvana <arpitmakvana192@gmail.com>
2024-07-26 15:50:06 +0530

    update

--- src/app/account/register/register.page.ts
+++ src/app/account/register/register.page.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterPage implements OnInit {
   emailPhoneFormData:any;
   nameDobFormData:any;
-  steps: number = 5;
+  steps: number = 0;
 
   constructor() { }
 
