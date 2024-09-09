0766ff7848ce84be5f235cc559aaa0cfcbf06b6c
ArpitMakvana <arpitmakvana192@gmail.com>
2024-09-09 19:21:43 +0530

    added socket

--- src/app/home-page/userchat-list/userchat-list.component.ts
+++ src/app/home-page/userchat-list/userchat-list.component.ts
@@ -1,14 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { SocketService } from '../../services/socket.service'; // Import the socket service
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-userchat-list',
   templateUrl: './userchat-list.component.html',
   styleUrls: ['./userchat-list.component.scss'],
 })
-export class UserchatListComponent  implements OnInit {
+export class UserchatListComponent implements OnInit, OnDestroy {
+  chatList: any = []; // Store chat list here
+  private chatListSubscription!: Subscription; // Subscription to manage cleanup
 
-  constructor() { }
+  constructor(private socketService: SocketService) {} // Inject SocketService
 
-  ngOnInit() {}
+  ngOnInit() {
+    // Subscribe to chatList event from socket
+    this.chatListSubscription = this.socketService.onEvent('chatList').subscribe(
+      (chatListData) => {
+        console.log('Chat List received:', chatListData); // Log received data
+        this.chatList = chatListData; // Store chat list data
+      },
+      (err) => {
+        console.error('Error receiving chat list:', err); // Handle error
+      }
+    );
+  }
 
+  ngOnDestroy() {
+    // Unsubscribe from the chatList event to avoid memory leaks
+    if (this.chatListSubscription) {
+      this.chatListSubscription.unsubscribe();
+    }
+  }
 }
